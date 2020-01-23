5cd22e8e7c4e07213dfa38da7d0459af2d01605b
jason_casse <jasoncasse@yahoo.com>
2020-01-22 19:03:49 -0700

    transfer for Mac

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 console.log("Up and running!");
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+console.log("User flipped queen");
+console.log("User flipped king");
 
+
+const cards = ["queen", "queen", "king", "king"]
+
+const cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped" + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	alert("You found a match!");
+
+} 
+else if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+}
+
+else { alert("Sorry, try again.")};
